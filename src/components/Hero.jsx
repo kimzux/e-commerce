a18bd06fee88324a68b8b94c8aeccd30caeafd3c
kimzux <kimzux0718@gmail.com>
2023-01-25 16:24:19 +0300

    final commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,19 +2,20 @@ import React from 'react'
 import image from '../assets/image-product.png';
 import vector from '../assets/Vector.png';
 import { AiOutlineShopping } from 'react-icons/ai'
-function Hero() {
+const Hero = ()=> {
   return (
     <div className='bg-dark-white flex flex-col'>
-    <div className='flex  mt-[100px]'>
+    <div className='flex  mt-[120px]'>
             <div className='flex flex-col'>
-            <img src={vector} alt=" profile" className="h-[200px] ml-[0px]"/>
-          <img src={vector} alt=" profile" className="h-[200px] x]"/>
-          <img src={vector} alt=" profile" className="h-[200px] x]"/>
+            <img src={vector} alt=" profile" className="w-[200px] "/>
+          <img src={vector} alt=" profile" className="w-[200px] ]"/>
+          <img src={vector} alt=" profile" className="w-[200px] ]"/>
+          <img src={vector} alt=" profile" className="w-[200px] ]"/>
             </div>
           
-            <div className='flex flex-col ml-[290px] mt-[50px]'>
+            <div className='flex flex-col ml-[200px] mt-[50px]'>
               <h2 className='text-[50px] font-light'>Collections</h2>
-              <p className='mt-[32px] text-[20px] mr-[200px] font-normal'>you can explore Kernel shop many different collections
+              <p className='mt-[32px] text-[20px] mr-[20px] font-normal'>you can explore Kernel shop many different collections
                 from various brands here.</p>
               <div className='flex mt-[30px]'>
                 <button className='text-white w-fix px-8 my-2 flex items-center rounded-md bg-orange cursor-pointer active:scale-[.98] active:duration-50 transition-all hover:scale-[1.01]  ease-in-out  transform py-4  '><div className='flex space-x-1'>
@@ -25,11 +26,11 @@ function Hero() {
               
             </div>
             
-            <img src={image} alt=" profile" className="w-[500px] h-[500px] mr-[300px]"/>
+            <img src={image} alt=" profile" className="w-[500px] h-[500px] mr-[200px]"/>
             <div className='flex flex-col'>
-            <img src={vector} alt=" profile" className="h-[200px] mr-[50px]"/>
-            <img src={vector} alt=" profile" className="h-[200px] mr-[50px]"/>
-            <img src={vector} alt=" profile" className="h-[200px] mr-[50px]"/>
+            <img src={vector} alt=" profile" className="w-[200px] mr-[50px]"/>
+            <img src={vector} alt=" profile" className="w-[200px] mr-[50px]"/>
+            <img src={vector} alt=" profile" className="w-[200px] mr-[50px]"/>
             </div>
            
           </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,15 +9,19 @@ const Navbar = () => {
       <header>
         <div className='bg-dark-white flex flex-col'>
           <div className='flex justify-center items-center relative mt-[10px]'>
-            <div className='absolute left-[300px]'>
-              <BiSearch size={20} />
+            <div className='absolute left-[300px] '>
+            <input class="bg-dark-white focus:bg-transparent outline-none py-2 px-4 block w-full appearance-none leading-normal mb-[10px]"  type="text"/>
+            <div className='flex absolute left-[0px] mt-[10px] inset-y-0 right-0 pr-3 pointer-events-none '> 
+             <BiSearch size={20} /> 
+             </div>
             </div>
+        
             <img src={logo} alt="my profile" className="w-[150px]" />
-            <div className='flex space-x-1 absolute left-[1260px] '>
+            <div className='flex space-x-1 absolute left-[1000px] '>
               <AiOutlineUser size={20} />
               <span>Account</span>
             </div>
-            <div className='flex space-x-1 absolute left-[1360px]'>
+            <div className='flex space-x-1 absolute left-[1100px]'>
               <AiOutlineShopping size={20} />
               <span>Shoping</span>
             </div>
@@ -26,7 +30,7 @@ const Navbar = () => {
             <hr class="w-64 h-px my-8 bg-gray border-0 dark:bg-gray-700" />
             <span class="absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-1/2 dark:text-white dark:bg-gray-900">shop</span>
           </div>
-          <div className='flex justify-center items-center space-x-20 text-[25px]'>
+          <div className='flex justify-center items-center space-x-20 text-[20px]'>
             <a href='home'>Home</a>
             <a href='home'>Products</a>
             <a href='home'>Collections</a>
